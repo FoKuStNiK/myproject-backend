6cab6bd247cd07e55f9570949c5d1c71c03f999c
FoKuStNiK <accforpcmi@gmail.com>
2026-08-18 15:31:28 +0300

    Подключил обновлённый WebSocket модуль

--- server/src/server.js
+++ server/src/server.js
@@ -3,15 +3,14 @@ const http = require('http');
 const cors = require('cors');
 const { WebSocketServer } = require('ws');
 const routes = require('./routes');
-const db = require('./db'); // ← импортируем db, а не initDb
-const setupTableSocket = require('./websocket/tableSocket');
+const db = require('./db');
+const { setupTableSocket } = require('./websocket/tableSocket');
 const PORT = 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/', routes);
 
 const server = http.createServer(app);
@@ -19,7 +18,6 @@ const wss = new WebSocketServer({ server });
 
 setupTableSocket(wss);
 
-// Просто запускаем сервер, не ждём БД
 server.listen(PORT, () => {
     console.log(`✅ Сервер на http://localhost:${PORT}`);
 });
