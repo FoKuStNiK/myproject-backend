a23751d2e499b4b5959976158a1936fc642370e0
FoKuStNiK <accforpcmi@gmail.com>
2026-08-20 20:17:59 +0300

    Убрал HTTP очистку таблицы

--- server/src/routes/tableRoutes.js
+++ server/src/routes/tableRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getTable, updateTableCell, clearTableData } = require('../controllers/tableController');
+const { getTable, updateTableCell } = require('../controllers/tableController');
 
 router.get('/table-data', getTable);
 router.post('/table-data/cell', updateTableCell);
-router.delete('/table-data', clearTableData);
 
 module.exports = router;
