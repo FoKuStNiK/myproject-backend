743f210e403436454799dbc8b197a515258d406d
FoKuStNiK <accforpcmi@gmail.com>
2026-08-20 12:02:34 +0300

    Подключил POST маршрут таблицы

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const newsRoutes = require('./newsRoutes');
 const achievementsRoutes = require('./achievementsRoutes');
 const supportRoutes = require('./supportRoutes');
+const tableRoutes = require('./tableRoutes');
 
 router.use('/api', newsRoutes);
 router.use('/api', achievementsRoutes);
 router.use('/api', supportRoutes);
+router.use('/api', tableRoutes);
 
 module.exports = router;
